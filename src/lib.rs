c3e580a4470c7a49e06c533cc8f4939f5bd50abe
muno <58895791+muno9748@users.noreply.github.com>
2022-09-17 17:44:41 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,13 @@
 #![cfg_attr(feature = "gat", feature(generic_associated_types))]
 
 #[cfg(feature = "encryption")]
-mod cfb8;
-mod connection;
-mod reader;
-mod tcp;
-mod util;
-mod wrapper;
-mod writer;
+pub mod cfb8;
+pub mod connection;
+pub mod reader;
+pub mod tcp;
+pub mod util;
+pub mod wrapper;
+pub mod writer;
 
 #[cfg(feature = "encryption")]
 pub use cfb8::CipherError;
